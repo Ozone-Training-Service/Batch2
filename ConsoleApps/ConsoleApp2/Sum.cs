8b1fa13e345a0c393bba1991b45ce696dc214805
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-02 11:10:29 +0530

    Sum of two number

---
+++ ConsoleApps/ConsoleApp2/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+
+
+
+
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+           
+             int a, b, c;
+            Console.WriteLine("Enter 1st num");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2st num");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Ans={0}",c);
+
+            Console.ReadKey();
+        }
+    }
+}
