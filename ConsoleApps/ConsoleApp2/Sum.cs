62f9c7f1860f0286cc90261807b29d1eb01e5436
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-02 11:22:37 +0530

    Calc

--- ConsoleApps/ConsoleApp2/Sum.cs
+++ ConsoleApps/ConsoleApp2/Sum.cs
@@ -5,21 +5,54 @@ using System.Text;
 namespace ConsoleApp2
 {
 
+   public  class Calc
+    {
+        public int x, y, z;
 
+        public   void input()
+        {
+            Console.WriteLine("Enter 1st num");
+            x= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd num");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Add()
+        {
+            z = x + y;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Sum={0}",z);
+        }
+    }
 
 
     class Sum
     {
         static void Main(string[] args)
         {
+
+
+            Calc ob = new Calc();
+            ob.input();
+            ob.Add();
+            ob.Display();
+
+            
+            
+            //int a, b, c;
+            //Console.WriteLine("Enter 1st num");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 2st num");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //c = a + b;
+            //Console.WriteLine("Ans={0}",c);
+
+
            
-             int a, b, c;
-            Console.WriteLine("Enter 1st num");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2st num");
-            b = Convert.ToInt32(Console.ReadLine());
-            c = a + b;
-            Console.WriteLine("Ans={0}",c);
 
             Console.ReadKey();
         }
