0565dd388229c2d37397e3b8d9a26f11bb2aba0a
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-05 10:25:51 +0530

    Multi level

---
+++ ConsoleApps/Inheritance/MultilevelInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class P
+    {
+       
+       public  void hello()
+        {
+            Console.WriteLine("Hello from P");
+        }
+
+    }
+    class Q:P
+    {
+       public  void Sum()
+        {
+            Console.WriteLine("Hello  from Q" );
+        }
+
+    }
+    class R:Q
+    {
+        public void Sub()
+        {
+            Console.WriteLine("Hello from sub");
+        }
+    }
+
+
+
+
+
+    public class MultilevelInheritance
+    {
+        static void Main(string[] args)
+        {
+
+            R o = new R();
+            o.hello();
+            o.Sub();
+            o.Sum();
+
+            Console.ReadKey();
+        }
+    }
+}
