61e7cd8295427f35b6b5836561c60055bcd13b0f
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-08 10:24:52 +0530

    FirstWeb App

---
+++ WebSites/FirstWebApp/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+
+
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
