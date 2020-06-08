84a9d477e3482ababac324ce952766240502a102
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-08 10:02:57 +0530

    try catch

---
+++ ConsoleApps/ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+                Console.WriteLine("Ans" + c);
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("B can't be 0");
+                Console.WriteLine(ex.Message);
+
+
+            }
+            finally
+            {
+                Console.WriteLine("hello hai goodmorning");
+            
+            }
+            
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+
+        }
+    }
+}
