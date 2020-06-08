bfc4259cf1ce48c49a22cfdde6de654d6fa8258e
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-08 11:25:31 +0530

    ViewBag and View Data

---
+++ WebSites/SecondApp/WebApp2/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2.Controllers
+{
+
+    //localhost:22002/Student/StudentList
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.Name1 = "Anu";
+            ViewBag.Name2 = "Liya";
+
+            ViewData["Name"] = "Vishnu";
+            ViewData["age"] = "090";
+
+            return View();
+        }
+
+
+        public IActionResult StudentList()//Action Method
+        {
+
+
+            return View();
+        }
+
+    }
+}
