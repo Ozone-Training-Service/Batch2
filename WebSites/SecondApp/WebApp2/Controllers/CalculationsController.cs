851da93e1a8f0120629f1e936b1a7f0686479103
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-10 12:04:55 +0530

    Calculations

---
+++ WebSites/SecondApp/WebApp2/Controllers/CalculationsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2.Controllers
+{
+    public class CalculationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ans(string n1, string n2)
+        {
+
+            int a = Convert.ToInt32(n1);
+            int b= Convert.ToInt32(n1);
+
+            int Sum = a + b;
+            int dif = a - b;
+            int q = a / b;
+            int p = a * b;
+
+            ViewBag.Sum = Sum;
+            ViewBag.dif = dif;
+            ViewBag.q = q;
+            ViewBag.p = p;
+            return View();
+        }
+    }
+}
