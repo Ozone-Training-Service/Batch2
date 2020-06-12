fdc9b0eb7baf94e6a59eca2765701a52a7b75ffb
anu.cs1995@gmail.com <anu.cs1995@gmail.com>
2020-06-12 09:44:34 +0530

    feedback anu

---
+++ StudentsWorks/Anu/WebSites/TestWebApp_1/Controllers/FeedbackController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApp_1.Models;
+
+namespace TestWebApp_1.Controllers
+{
+	public class FeedbackController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Feed(Feedback feedback)
+		{
+			ViewBag.Name = feedback.Name;
+			ViewBag.Message = feedback.Message;
+			ViewBag.Date = feedback.Date;
+			return View();
+
+
+		}
+	}
+}
---
+++ StudentsWorks/Anu/WebSites/TestWebApp_1/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp_1.Models
+{
+	public class Feedback
+	{
+		public string Name { get; set; }
+		public string Message { get; set; }
+		public int Date { get; set; }
+	}
+}
