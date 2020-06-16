5a274c9480692f02dcf5c228a4cd891f28b347e1
anu.cs1995@gmail.com <anu.cs1995@gmail.com>
2020-06-16 10:21:46 +0530

    TemplatesWrok By anu

---
+++ AddNewTemplate/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddNewTemplate.Models
+{
+	public class Register
+	{
+		public string Name { get; set; }
+		public string Birthdate { get; set; }
+		public string Gender { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+	}
+}
