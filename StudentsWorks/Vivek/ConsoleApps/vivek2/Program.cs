5a1ad9b3ed69d9754344ff78797cd909dbe2aa33
vivekps561 <vivekps561@gmail.com>
2020-06-19 12:12:38 +0530

    calc app

---
+++ StudentsWorks/Vivek/ConsoleApps/vivek2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace vivek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum, sub, mul, div;
+            Console.WriteLine("Enter two numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            sub = a - b;
+            mul = a * b;
+            
+            
+.            Console.WriteLine("sum =" + sum);
+           
+        }
+    }
+}
