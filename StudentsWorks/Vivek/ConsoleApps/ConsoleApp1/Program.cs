8560bbaaffcc9af1a6110f8684c5e7fab17a28ee
vivekps561 <vivekps561@gmail.com>
2020-06-19 12:23:25 +0530

    even or odd

---
+++ StudentsWorks/Vivek/ConsoleApps/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("enter the number");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a%2==0)
+            {
+                Console.WriteLine("entered number is even");
+            }
+            else {
+                Console.WriteLine("entered number is odd");
+            }
+            
+        }
+    }
+}
